1fbae69eca57011583b261ddebb10015bc087e89
demiralp <demiralp@vr.rwth-aachen.de>
2017-12-08 11:09:53 +0100

    State contains a transition vector.

--- include/mcmc/markov_chain_state.hpp
+++ include/mcmc/markov_chain_state.hpp
@@ -1,8 +1,12 @@
 #ifndef MCMC_MARKOV_CHAIN_STATE_HPP_
 #define MCMC_MARKOV_CHAIN_STATE_HPP_
 
+#include <array>
+#include <cstddef>
+
 namespace mcmc
 {
+template <typename probability_type, std::size_t state_space_size>
 class markov_chain_state
 {
 public:
@@ -14,7 +18,7 @@ public:
   markov_chain_state& operator=(      markov_chain_state&& temp) = default;
 
 protected:
-
+  std::array<probability_type, state_space_size> transition_vector_;
 };
 }
 
