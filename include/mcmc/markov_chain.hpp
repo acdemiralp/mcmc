9036b01aa334fe9c1659af25617d6e11820a9a72
demiralp <demiralp@vr.rwth-aachen.de>
2017-12-13 11:30:17 +0100

    Implemented a generic markov chain which takes in a state type (e.g. array of N values) and an update strategy (e.g. Metropolis-Hastings).

--- include/mcmc/markov_chain.hpp
+++ include/mcmc/markov_chain.hpp
@@ -1,20 +1,41 @@
 #ifndef MCMC_MARKOV_CHAIN_HPP_
 #define MCMC_MARKOV_CHAIN_HPP_
 
+#include <vector>
+
 namespace mcmc
 {
+template<typename state_type>
 class markov_chain
 {
 public:
-  markov_chain           ()                          = default;
+  explicit markov_chain  (state_type initial_state ) : state_history_{initial_state}
+  {
+    
+  }
   markov_chain           (const markov_chain&  that) = default;
   markov_chain           (      markov_chain&& temp) = default;
   virtual ~markov_chain  ()                          = default;
   markov_chain& operator=(const markov_chain&  that) = default;
   markov_chain& operator=(      markov_chain&& temp) = default;
 
-protected:
+  template<typename update_strategy, typename... argument_types>
+  void update(argument_types&&... arguments)
+  {
+    state_history_.push_back(update_strategy::apply(state_history_[state_history_.size() - 1], arguments...));
+  }
 
+  const state_type&              state        () const
+  {
+    return state_history_.back();
+  }
+  const std::vector<state_type>& state_history() const
+  {
+    return state_history_;
+  }
+
+protected:
+  std::vector<state_type> state_history_;
 };
 }
 
--- tests/markov_chain_test.cpp
+++ tests/markov_chain_test.cpp
@@ -1,8 +1,57 @@
 #include "catch.hpp"
 
+#include <algorithm>
+#include <array>
+
 #include <mcmc/markov_chain.hpp>
 
 TEST_CASE("Markov chain is tested.", "[mcmc::markov_chain]") 
 {
+  GIVEN("A Markov Chain with four floating point states with initial probabilities of 0.1, 0.2, 0.3, 0.4 respectively.")
+  {
+    std::array<float, 4>                     initial_state {0.1F, 0.2F, 0.3F, 0.4F};
+    mcmc::markov_chain<std::array<float, 4>> markov_chain  (initial_state);
 
+    THEN("The state history size should be equal to one.")
+    {
+      REQUIRE(markov_chain.state_history().size() == 1);
+    }
+    THEN("The state should be equal to the initial state.")
+    {
+      REQUIRE(markov_chain.state() == initial_state);
+    }
+    THEN("The last entry in the state history should be equal to the initial state.")
+    {
+      REQUIRE(markov_chain.state_history().back() == initial_state);
+    }
+
+    WHEN("The state is updated using a trivial update strategy which only inverts the state.")
+    {
+      struct state_inversion_strategy
+      {
+        static std::array<float, 4> apply(std::array<float, 4> state)
+        {
+          std::reverse(state.begin(), state.end());
+          return state;
+        }
+      };
+      markov_chain.update<state_inversion_strategy>();
+
+      auto inverted_state = initial_state;
+      std::reverse(inverted_state.begin(), inverted_state.end());
+
+      THEN("The state history size should be equal to two.")
+      {
+        REQUIRE(markov_chain.state_history().size() == 2);
+      }
+      THEN("The state should be equal to the inverted state.")
+      {
+        REQUIRE(markov_chain.state() == inverted_state);
+      }
+      THEN("The last entry in the state history should be equal to the inverted state.")
+      {
+        REQUIRE(markov_chain.state_history().back() == inverted_state);
+      }
+    }
+  }
 }
