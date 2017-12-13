307d45e026ac8a099a7633a839c06e71ee642b0b
demiralp <demiralp@vr.rwth-aachen.de>
2017-12-13 11:30:37 +0100

    Metropolis-Hastings sampler progress.

--- include/mcmc/metropolis_hastings_sampler.hpp
+++ include/mcmc/metropolis_hastings_sampler.hpp
@@ -8,15 +8,16 @@ namespace mcmc
 {
 template<
   typename type, 
-  typename prior_distribution      = std::gamma_distribution <type>, 
-  typename likelihood_distribution = std::normal_distribution<type>>
+  typename prior_distribution_type      = std::gamma_distribution <type>, 
+  typename likelihood_distribution_type = std::normal_distribution<type>>
 class metropolis_hastings_sampler
 {
 public:
-  metropolis_hastings_sampler           ()
+  metropolis_hastings_sampler(prior_distribution_type prior_distribution, likelihood_distribution_type likelihood_distribution) 
+  : prior_distribution_(prior_distribution), likelihood_distribution_(likelihood_distribution)
   {
-    static_assert(!std::is_function<prior_distribution>::value     , "Prior distribution is not a function."     );
-    static_assert(!std::is_function<likelihood_distribution>::value, "Likelihood distribution is not a function.");
+    static_assert(!std::is_function<prior_distribution_type>::value     , "Prior distribution is not a function."     );
+    static_assert(!std::is_function<likelihood_distribution_type>::value, "Likelihood distribution is not a function.");
   }
   metropolis_hastings_sampler           (const metropolis_hastings_sampler&  that) = default;
   metropolis_hastings_sampler           (      metropolis_hastings_sampler&& temp) = default;
@@ -25,7 +26,8 @@ public:
   metropolis_hastings_sampler& operator=(      metropolis_hastings_sampler&& temp) = default;
 
 protected:
-
+  prior_distribution_type      prior_distribution_     ;
+  likelihood_distribution_type likelihood_distribution_;
 };
 }
 
