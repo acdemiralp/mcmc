77baf2b0d920241cbdcbc40bb6bc03d0e90d2c10
Demiralp <demiralpali@gmail.com>
2018-04-04 02:56:02 +0200

    Metropolis-Hastings sampler functional. Test demonstrates estimating a normal distribution with in-situ output.

--- include/mcmc/metropolis_hastings_sampler.hpp
+++ include/mcmc/metropolis_hastings_sampler.hpp
@@ -2,30 +2,38 @@
 #define MCMC_METROPOLIS_HASTINGS_SAMPLER_HPP_
 
 #include <algorithm>
+#include <math.h>
 #include <random>
 #include <type_traits>
+
+#include <external/Eigen/Cholesky>
+#include <external/Eigen/Dense>
 
 #include <mcmc/random_number_generator.hpp>
 
 namespace mcmc
 {
 template<
-  typename data_type                    ,
-  typename parameter_type               ,
-  typename model_function_type          ,
-  typename prior_distribution_type      = std::gamma_distribution <double>, 
-  typename likelihood_distribution_type = std::normal_distribution<double>>
+  typename kernel_function_type       ,
+  typename state_type                 = Eigen::VectorXf,
+  typename covariance_matrix_type     = Eigen::MatrixXf,
+  typename proposal_distribution_type = std::normal_distribution<float>>
 class metropolis_hastings_sampler
 {
 public:
+  template<typename... arguments_type>
   explicit metropolis_hastings_sampler  (
-    prior_distribution_type      prior_distribution      = prior_distribution_type     (), 
-    likelihood_distribution_type likelihood_distribution = likelihood_distribution_type()) 
-  : prior_distribution_     (prior_distribution     )
-  , likelihood_distribution_(likelihood_distribution)
+    const std::function<float(const state_type&)>& kernel_function       ,
+    const covariance_matrix_type&                  covariance_matrix     ,
+    const float                                    scale                 = 1.0f,
+    const proposal_distribution_type&              proposal_distribution = proposal_distribution_type())
+  : kernel_function_  (kernel_function      )
+  , covariance_matrix_((std::pow(scale, 2) * covariance_matrix).llt().matrixLLT())
+  , proposal_rng_     (proposal_distribution)
+  , accept_rng_       (0.0f, 1.0f           )
+  , last_density_     (0.0f                 )
   {
-    static_assert(!std::is_function<prior_distribution_type>::value     , "Prior distribution is not a function."     );
-    static_assert(!std::is_function<likelihood_distribution_type>::value, "Likelihood distribution is not a function.");
+
   }
   metropolis_hastings_sampler           (const metropolis_hastings_sampler&  that) = default;
   metropolis_hastings_sampler           (      metropolis_hastings_sampler&& temp) = default;
@@ -33,25 +41,27 @@ public:
   metropolis_hastings_sampler& operator=(const metropolis_hastings_sampler&  that) = default;
   metropolis_hastings_sampler& operator=(      metropolis_hastings_sampler&& temp) = default;
 
-  template<typename state_type>
-  state_type apply(const state_type& state) const
+  state_type apply (const state_type& state)
   {
-    // Pick from posterior distribution g(x' | x_t).
-    const auto xp = 0.0F;
+    state_type  random_vector(state.size());
+    std::generate_n(&random_vector[0], random_vector.size(), proposal_rng_.function());
 
-    // Calculate acceptance ratio.
-    const auto a  = 0.0F;
+    state_type  next_state = state + covariance_matrix_ * random_vector;
+    const float density    = kernel_function_(next_state);
+
+    if (std::exp(std::min(0.0f, density - last_density_)) < accept_rng_.generate()) 
+      return state;
     
-    // Accept or reject step.
-    return uniform_rng_.generate() <= a ? xp : state;
+    last_density_ = density;
+    return next_state;
   }
 
 protected:
-  random_number_generator<type, prior_distribution_type>      uniform_rng_            ;
-  random_number_generator<type, likelihood_distribution_type> normal_rng_             ;
-  model_function_type                                         model_function_         ;
-  prior_distribution_type                                     prior_distribution_     ;
-  likelihood_distribution_type                                likelihood_distribution_;
+  std::function<float(const state_type&)>                        kernel_function_  ;
+  covariance_matrix_type                                         covariance_matrix_;
+  random_number_generator<proposal_distribution_type>            proposal_rng_     ;
+  random_number_generator<std::uniform_real_distribution<float>> accept_rng_       ;
+  float                                                          last_density_     ;
 };
 }
 
--- tests/metropolis_hastings_sampler_test.cpp
+++ tests/metropolis_hastings_sampler_test.cpp
@@ -1,14 +1,9 @@
 #include "catch.hpp"
 
-#include <array>
-#include <cstddef>
-#include <fstream>
-#include <random>
+#include <iostream>
 
+#include <mcmc/markov_chain.hpp>
 #include <mcmc/metropolis_hastings_sampler.hpp>
-
-#define M_LN_SQRT_2PI	0.918938533204672741780329736406
-#define M_1_SQRT_2PI	0.398942280401432677939946059934
 
 double normal_distribution_density(
   double       x           , 
@@ -17,101 +12,30 @@ double normal_distribution_density(
   const int    logarithmic = false)
 {
   x = fabs((x - mu) / sigma);
-  return logarithmic ? -(M_LN_SQRT_2PI + 0.5 * x * x + log(sigma)) : M_1_SQRT_2PI * exp(-0.5 * x * x) / sigma;
+  return logarithmic ? -(0.918938533204672741780329736406 + 0.5 * x * x + log(sigma)) : 0.398942280401432677939946059934 * exp(-0.5 * x * x) / sigma;
 }
 
 TEST_CASE("Metropolis-Hastings sampler is tested.", "[mcmc::metropolis_hastings_sampler]")
 {
-  // At the minimum, it is necessary to know how to compute the density of the distribution.
-  // The posterior is proportional to likelihood * prior.
+  Eigen::VectorXf initial_state(1);
+  initial_state[0] = 150.0f;
+  
+  Eigen::MatrixXf covariance_matrix(1, 1);
+  covariance_matrix.setIdentity();
 
-  std::random_device                    random_device            ;
-  std::mt19937                          mersenne_twister         (random_device());
-  std::uniform_real_distribution<float> uniform_real_distribution(0.0f, 1.0f);
-  std::normal_distribution      <float> normal_distribution      (0.0f, 5.0f);
-
-  std::vector<std::size_t> samples(50000);
-  samples[0] = 110;
-  for (auto i = 1; i < samples.size(); ++i)
+  auto kernel_function = [ ] (const Eigen::VectorXf state) -> float 
   {
-    const auto proposal = samples[i - 1] + normal_distribution(mersenne_twister);
-    samples[i] = normal_distribution_density(proposal, 100, 15) / normal_distribution_density(samples[i - 1], 100, 15) >
-                 uniform_real_distribution(mersenne_twister)
-                   ? samples[i] = proposal
-                   : samples[i] = samples[i - 1];
-  }
+    auto sum = 0.0f;
+    for (auto i = 0; i < state.size(); ++i)
+      sum += normal_distribution_density(state[0], 500.0f, 100.0f, true);
+    return sum;
+  };
 
-  std::ofstream file("samples.txt");
-  for (auto i = 0; i < samples.size(); ++i)
-    file << samples[i] << std::endl;
-}
-
-
-
-
-
-using parameters = std::array<float, 2>; // d, C
-
-struct data
-{
-  std::size_t present      = 1000;
-  std::size_t absent       =  100;
-  std::size_t hits         =   85;
-  std::size_t false_alarms =   12;
-};
-
-auto posterior_density = [ ] (const parameters& parameters, const data& data)
-{
-  // TODO
-  return 0.0F;
-};
-
-TEST_CASE("Metropolis Test", "[metropolis]")
-{
-  std::random_device                          random_device                               ;
-  std::mt19937                                mersenne_twister         (random_device  ());
-  const std::uniform_real_distribution<float> uniform_real_distribution(0.0F, 1.0F       );
-
-  std::random_device                          random_device_2                             ;
-  std::mt19937                                mersenne_twister_2       (random_device_2());
-  std::normal_distribution<float>             normal_distribution      (0.0F, 0.1F       ); // Adjust to application or even per parameter.
-
-  data                    data;
-  std::vector<parameters> samples(500);
-  samples[0] = {0.5F, 1.0F};
-  for (auto i = 1; i < samples.size(); ++i)
+  mcmc::metropolis_hastings_sampler<decltype(kernel_function)> metropolis_hastings_sampler(kernel_function, covariance_matrix, 1.0f);
+  mcmc::markov_chain<Eigen::VectorXf>                          markov_chain               (initial_state);
+  for(auto i = 0; i < 1000000; ++i)
   {
-    auto proposal = samples[i - 1];
-    for (auto& sample : proposal)
-      sample += normal_distribution(mersenne_twister_2);
-    samples[i] = posterior_density(proposal, data) / posterior_density(samples[i - 1], data) > uniform_real_distribution(mersenne_twister)
-      ? proposal
-      : samples[i - 1];
-  }
-}
-
-TEST_CASE("Gibbs Test", "[gibbs]")
-{
-  std::random_device                          random_device                               ;
-  std::mt19937                                mersenne_twister         (random_device  ());
-  const std::uniform_real_distribution<float> uniform_real_distribution(0.0F, 1.0F       );
-
-  std::random_device                          random_device_2                             ;
-  std::mt19937                                mersenne_twister_2       (random_device_2());
-  std::normal_distribution<float>             normal_distribution      (0.0F, 0.1F       ); // Adjust to application or even per parameter.
-
-  data                    data;
-  std::vector<parameters> samples(500);
-  samples[0] = {0.5F, 1.0F};
-  for (auto i = 1; i < samples.size(); ++i)
-  {
-    samples[i] = samples[i - 1];
-    for (auto j = 0; j < samples[i].size(); ++j)
-    {
-      auto proposal = samples[i];
-      proposal[j] += normal_distribution(mersenne_twister_2);
-      if (posterior_density(proposal, data) / posterior_density(samples[i], data) > uniform_real_distribution(mersenne_twister))
-        samples[i] = proposal;
-    }
+    markov_chain.update(metropolis_hastings_sampler);
+    std::cout << markov_chain.state().format(Eigen::IOFormat()) << "\n"; // In-situ.
   }
 }
