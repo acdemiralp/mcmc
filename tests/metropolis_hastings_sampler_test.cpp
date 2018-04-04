5334f472fded2f4226fd6082acbb7de2c6b7d934
Demiralp <demiralpali@gmail.com>
2018-04-04 20:48:40 +0200

    Fiddling with the MH test.

--- tests/metropolis_hastings_sampler_test.cpp
+++ tests/metropolis_hastings_sampler_test.cpp
@@ -1,5 +1,6 @@
 #include "catch.hpp"
 
+#include <fstream>
 #include <iostream>
 
 #include <mcmc/markov_chain.hpp>
@@ -18,23 +19,29 @@ double normal_distribution_density(
 TEST_CASE("Metropolis-Hastings sampler is tested.", "[mcmc::metropolis_hastings_sampler]")
 {
   Eigen::VectorXf initial_state(1);
-  initial_state[0] = 400.0f;
+  initial_state[0] = 450.0f;
   
   Eigen::MatrixXf covariance_matrix(1, 1);
   covariance_matrix.setIdentity();
 
-  mcmc::metropolis_hastings_sampler<Eigen::VectorXf, Eigen::VectorXf, std::fisher_f_distribution<float>> metropolis_hastings_sampler(
+  mcmc::metropolis_hastings_sampler<Eigen::VectorXf, Eigen::MatrixXf, std::normal_distribution<float>> metropolis_hastings_sampler(
     [ ] (const Eigen::VectorXf state)
     {
-      return normal_distribution_density(state[0], 500.0f, 0.1f, true);
+      return normal_distribution_density(state[0], 500.0f, 1.0f, true);
     },
     covariance_matrix, 
     100.0f);
 
+  const auto iterations = 100000;
+
   mcmc::markov_chain<Eigen::VectorXf> markov_chain(initial_state);
-  for(auto i = 0; i < 1000000; ++i)
+  for(auto i = 0; i < iterations; ++i)
   {
     markov_chain.update(metropolis_hastings_sampler);
     std::cout << markov_chain.state().format(Eigen::IOFormat()) << "\n"; // In-situ.
   }
+
+  std::ofstream file("output.csv");
+  for (auto state : markov_chain.state_history())
+    file << state.format(Eigen::IOFormat()) << "\n";
 }
