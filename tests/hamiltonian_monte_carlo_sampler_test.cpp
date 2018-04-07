4b49f18b49308838a8ab232a5567df5b9bc8a36a
Demiralp <demiralpali@gmail.com>
2018-04-07 20:02:41 +0200

    Updated HMC test.

--- tests/hamiltonian_monte_carlo_sampler_test.cpp
+++ tests/hamiltonian_monte_carlo_sampler_test.cpp
@@ -8,41 +8,45 @@
 #include <mcmc/samplers/hamiltonian_monte_carlo_sampler.hpp>
 #include <mcmc/markov_chain.hpp>
 
-float normal_distribution_density(const float mu, const float sigma, const Eigen::VectorXf& state, Eigen::VectorXf* gradients)
-{
-  if (gradients) 
-  {
-    (*gradients)[0] = (state.array() - mu).sum() / std::pow(sigma, 2);
-    (*gradients)[1] = (state.array() - mu).pow(2).sum() / std::pow(sigma, 3) - 2.0f / sigma;
-  }
-  return -2.0f * (0.5f * std::log(2.0f * M_PI) + std::log(sigma)) - ((state.array() - mu).pow(2) / (2.0f * std::pow(sigma, 2))).sum();
-}
-
 TEST_CASE("Hamiltonian Monte Carlo sampler is tested.", "[mcmc::hamiltonian_monte_carlo_sampler]")
 {
+  mcmc::random_number_generator<std::normal_distribution<float>> data_generator(10.0f, 5.0f);
+  const auto data = data_generator.generate<Eigen::VectorXf>(1000);
+  
   Eigen::VectorXf initial_state(2);
-  initial_state[0] = 2.0f; // Mean.
-  initial_state[1] = 1.0f; // Standard deviation.
-
+  initial_state[0] = 100.0f;
+  initial_state[1] = 100.0f;
+  
   Eigen::MatrixXf precondition_matrix(2, 2);
   precondition_matrix.setIdentity();
 
-  mcmc::hamiltonian_monte_carlo_sampler<Eigen::VectorXf> sampler(
-    [ ] (const Eigen::VectorXf& state, Eigen::VectorXf* gradients)
+  auto log_likelihood_density = [ ] (const Eigen::VectorXf& state, const Eigen::VectorXf& data, Eigen::VectorXf* gradients)
+  {
+    if(gradients)
     {
-      return normal_distribution_density(4.0f, 1.0f, state, gradients);
+      (*gradients)[0] = (data.array() - state[0])       .sum() / std::pow(state[1], 2);
+      (*gradients)[1] = (data.array() - state[0]).pow(2).sum() / std::pow(state[1], 3) - static_cast<float>(data.size()) / state[1];
+    }
+    return -static_cast<float>(data.size()) * (0.5f * std::log(2.0f * M_PI) + std::log(state[1])) - ((data.array() - state[0]).pow(2) / (2.0f * std::pow(state[1], 2))).sum();
+  };
+
+  mcmc::hamiltonian_monte_carlo_sampler<Eigen::VectorXf, Eigen::MatrixXf, std::normal_distribution<float>> sampler(
+    [=] (const Eigen::VectorXf& state, Eigen::VectorXf* gradients)
+    {
+      return log_likelihood_density(state, data, gradients);
     },
-    precondition_matrix,
-    1u,
-    1.0f);
+    precondition_matrix, 
+    20,
+    0.1f);
   sampler.setup(initial_state);
 
   mcmc::markov_chain<Eigen::VectorXf> markov_chain(initial_state);
-  for(auto i = 0; i < 10000; ++i)
+  for(auto i = 0; i < 1000; ++i)
   {
     markov_chain.update(sampler);
     std::cout << markov_chain.state().format(Eigen::IOFormat()) << "\n";
   }
 
-  REQUIRE(Approx(markov_chain.state()[0]).epsilon(1.0f) == 4.0f);
+  REQUIRE(Approx(markov_chain.state()[0]).epsilon(1.0f) == 10.0f);
+  REQUIRE(Approx(markov_chain.state()[1]).epsilon(1.0f) ==  5.0f);
 }
