620080248002092ef17dc87363b25241f9a9d031
Demiralp <demiralpali@gmail.com>
2018-05-11 19:27:51 +0200

    Riemannian manifold Hamiltonian Monte Carlo progress.

--- tests/riemannian_manifold_hamiltonian_monte_carlo_sampler_test.cpp
+++ tests/riemannian_manifold_hamiltonian_monte_carlo_sampler_test.cpp
@@ -5,40 +5,36 @@
 #include <math.h>
 #include <iostream>
 
-#include <mcmc/samplers/differential_evolution_sampler.hpp>
+#include <mcmc/samplers/riemannian_manifold_hamiltonian_monte_carlo_sampler.hpp>
 #include <mcmc/markov_chain.hpp>
 #include <mcmc/random_number_generator.hpp>
-#include "mcmc/samplers/independent_metropolis_hastings_sampler.hpp"
-#include "mcmc/samplers/metropolis_adjusted_langevin_sampler.hpp"
-#include "mcmc/samplers/riemannian_manifold_hamiltonian_monte_carlo_sampler.hpp"
 
 TEST_CASE("Riemannian manifold Hamiltonian Monte Carlo sampler is tested.", "[mcmc::riemannian_manifold_hamiltonian_monte_carlo_sampler]")
 {
-  mcmc::random_number_generator<std::normal_distribution<float>> data_generator(250.0f, 0.1f);
-  const auto data = data_generator.generate<Eigen::VectorXf>(100);
+  mcmc::random_number_generator<std::normal_distribution<float>> data_generator(10.0f, 5.0f);
+  const auto data = data_generator.generate<Eigen::VectorXf>(1000);
   
-  Eigen::VectorXf initial_state(1);
-  initial_state[0] = 1000.0f;
+  Eigen::VectorXf initial_state(2);
+  initial_state[0] = 100.0f;
+  initial_state[1] = 100.0f;
   
-  Eigen::MatrixXf covariance_matrix(1, 1);
-  covariance_matrix.setIdentity();
-
-  auto log_likelihood_density = [ ] (const Eigen::VectorXf& state, const Eigen::VectorXf& data, const float sigma = 1.0f)
-  {
-    return -static_cast<float>(data.size()) * (0.5f * std::log(2.0f * M_PI) + std::log(sigma)) - ((data.array() - state[0]).pow(2) / (2.0f * std::pow(sigma, 2))).sum();
-  };
-  auto log_prior_density      = [ ] (const Eigen::VectorXf& state, const float mu = 0.0f, const float sigma = 1.0f)
-  {
-    return -0.5f * std::log(2.0f * M_PI) - std::log(sigma) - std::pow(state[0] - mu, 2) / (2.0f * std::pow(sigma, 2));
-  };
-
-  mcmc::riemannian_manifold_hamiltonian_monte_carlo_sampler<Eigen::VectorXf, Eigen::MatrixXf, std::normal_distribution<float>> sampler(
-    [=] (const Eigen::VectorXf& state)
+  mcmc::riemannian_manifold_hamiltonian_monte_carlo_sampler<float, Eigen::VectorXf, Eigen::MatrixXf, Eigen::Tensor<float, 3>, std::normal_distribution<float>> sampler(
+    [=] (const Eigen::VectorXf& state, Eigen::VectorXf* gradients)
     {
-      return log_likelihood_density(state, data, 0.1f) + log_prior_density(state, 0.0f, 1.0f);
+      if(gradients)
+      {
+        (*gradients)[0] = (data.array() - state[0])       .sum() / std::pow(state[1], 2);
+        (*gradients)[1] = (data.array() - state[0]).pow(2).sum() / std::pow(state[1], 3) - static_cast<float>(data.size()) / state[1];
+      }
+      return -static_cast<float>(data.size()) * (0.5f * std::log(2.0f * M_PI) + std::log(state[1])) - ((data.array() - state[0]).pow(2) / (2.0f * std::pow(state[1], 2))).sum();
     },
-    covariance_matrix, 
-    1.0f);
+    [ ] (const Eigen::VectorXf& state, Eigen::Tensor<float, 3>* tensor)
+    {
+      return Eigen::MatrixXf();
+    },
+    20,
+    5,
+    0.1f);
   sampler.setup(initial_state);
 
   mcmc::markov_chain<Eigen::VectorXf> markov_chain(initial_state);
@@ -48,5 +44,6 @@ TEST_CASE("Riemannian manifold Hamiltonian Monte Carlo sampler is tested.", "[mc
     std::cout << markov_chain.state().format(Eigen::IOFormat()) << "\n";
   }
 
-  REQUIRE(Approx(markov_chain.state()[0]).epsilon(0.1) == 250.0f);
+  REQUIRE(Approx(markov_chain.state()[0]).epsilon(1.0f) == 10.0f);
+  REQUIRE(Approx(markov_chain.state()[1]).epsilon(1.0f) ==  5.0f);
 }
