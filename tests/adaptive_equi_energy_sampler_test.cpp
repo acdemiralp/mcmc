f55e66bf0086af628ff55f76830737454c8a50fb
Demiralp <demiralpali@gmail.com>
2018-05-20 19:41:27 +0200

    AEES complete.

--- tests/adaptive_equi_energy_sampler_test.cpp
+++ tests/adaptive_equi_energy_sampler_test.cpp
@@ -10,40 +10,55 @@
 #include <mcmc/random_number_generator.hpp>
 
 TEST_CASE("Adaptive equi-energy sampler is tested.", "[mcmc::adaptive_equi_energy_sampler]")
-{
-  mcmc::random_number_generator<std::normal_distribution<float>> data_generator(250.0f, 0.1f);
-  const auto data = data_generator.generate<Eigen::VectorXf>(100);
+{  
+  auto log_mixture_of_gaussians_density = [ ] (const Eigen::VectorXf& x, const Eigen::VectorXf& weights, const Eigen::MatrixXf& mean, const Eigen::VectorXf& variance)
+  {
+    auto density = 0.0f;
+    for (auto i = 0; i < weights.size(); i++)
+      density += weights[i] * std::exp(-0.5f * (x - mean.col(0)).array().pow(2).sum() / variance[i]) / std::pow(2.0f * M_PI * variance[i], static_cast<float>(x.size()) / 2.0f);
+    return std::log(density);
+  };
+
+  Eigen::VectorXf weights(2);
+  weights[0]       = 0.5f;
+  weights[1]       = 0.5f;
   
-  Eigen::VectorXf initial_state(1);
-  initial_state[0] = 1000.0f;
-  
-  Eigen::MatrixXf covariance_matrix(1, 1);
+  Eigen::MatrixXf mean(2, 2);
+  mean(0, 0)       = 2.0f;
+  mean(1, 0)       = 2.0f;
+  mean(0, 1)       = 1.0f;
+  mean(1, 1)       = 1.0f;
+
+  Eigen::VectorXf variance(2);
+  variance[0]      = 0.1f;
+  variance[1]      = 0.1f;
+
+  Eigen::VectorXf initial_state(2);
+  initial_state[0] = 2.0f;
+  initial_state[1] = 2.0f;
+
+  Eigen::VectorXf temperatures(2);
+  temperatures[0]  = 50.0f;
+  temperatures[1]  = 10.0f;
+
+  Eigen::MatrixXf covariance_matrix(2, 2);
   covariance_matrix.setIdentity();
+  covariance_matrix *= 0.35f;
 
-  auto log_likelihood_density = [ ] (const Eigen::VectorXf& state, const Eigen::VectorXf& data, const float sigma = 1.0f)
-  {
-    return -static_cast<float>(data.size()) * (0.5f * std::log(2.0f * M_PI) + std::log(sigma)) - ((data.array() - state[0]).pow(2) / (2.0f * std::pow(sigma, 2))).sum();
-  };
-  auto log_prior_density      = [ ] (const Eigen::VectorXf& state, const float mu = 0.0f, const float sigma = 1.0f)
-  {
-    return -0.5f * std::log(2.0f * M_PI) - std::log(sigma) - std::pow(state[0] - mu, 2) / (2.0f * std::pow(sigma, 2));
-  };
+  mcmc::adaptive_equi_energy_sampler<float, Eigen::VectorXf, Eigen::MatrixXf, std::normal_distribution<float>> sampler(
+    std::bind(log_mixture_of_gaussians_density, std::placeholders::_1, weights, mean, variance),
+    covariance_matrix,
+    1.0f,
+    0.1f,
+    10  );
 
-  mcmc::adaptive_equi_energy_sampler<Eigen::VectorXf, Eigen::MatrixXf, std::normal_distribution<float>> sampler(
-    [=] (const Eigen::VectorXf& state)
-    {
-      return log_likelihood_density(state, data, 0.1f) + log_prior_density(state, 0.0f, 1.0f);
-    },
-    covariance_matrix, 
-    1.0f);
-  sampler.setup(initial_state);
-
-  mcmc::markov_chain<Eigen::VectorXf> markov_chain(initial_state);
+  mcmc::markov_chain<Eigen::MatrixXf> markov_chain(sampler.setup(initial_state, temperatures, 1000, 10000));
   for(auto i = 0; i < 10000; ++i)
   {
     markov_chain.update(sampler);
     std::cout << markov_chain.state().format(Eigen::IOFormat()) << "\n";
   }
 
-  REQUIRE(Approx(markov_chain.state()[0]).epsilon(0.1) == 250.0f);
+  // Not trivial for mixed distributions.
+  // REQUIRE(Approx(markov_chain.state()[0]).epsilon(0.1) == 250.0f);
 }
